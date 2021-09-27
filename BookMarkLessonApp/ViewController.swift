e529370e8fd5467f104002c9d08c355cc6c85420
HiroUrata <1712026@i.softbank.jp>
2021-09-27 18:14:18 +0900

    UISearchBar 追加　ViewController

--- BookMarkLessonApp/ViewController.swift
+++ BookMarkLessonApp/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let searchBar = UISearchBar()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        
+        
     }
 
+    override func viewWillLayoutSubviews() {
+        
+        searchBar.frame = CGRect(x: self.view.frame.minX, y: self.view.frame.minY + self.view.safeAreaInsets.top, width: self.view.frame.size.width, height: 44)
+        searchBar.showsBookmarkButton = true
+        searchBar.showsCancelButton = true
+        view.addSubview(searchBar)
+        
+        
+    }
+    
 
 }
 
